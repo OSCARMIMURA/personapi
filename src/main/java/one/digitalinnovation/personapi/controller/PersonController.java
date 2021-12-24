733f97678486a162bbe14d6d03b221c7bcc9c7c8
oscarmimura <oscar.mimura@gmail.com>
2021-12-23 22:28:45 -0300

    DeleteById method

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -38,7 +38,45 @@ public class PersonController {
     }
 
     @GetMapping("/{id}")
-    public PersonDTO findById(Long id) throws PersonNotFoundException {
+    public PersonDTO findById(@PathVariable  Long id) throws PersonNotFoundException {
+        System.out.println("passei aqui" + id);
         return personService.findById (id);
     }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable Long id) throws PersonNotFoundException{
+        personService.delete(id);
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/one/digitalinnovation/personapi/service/PersonService.java
+++ src/main/java/one/digitalinnovation/personapi/service/PersonService.java
@@ -47,10 +47,22 @@ public class PersonService {
 
 
     public PersonDTO findById(Long id) throws PersonNotFoundException {
-        Optional<Person> optionalPerson =personRepository.findById(id);
-        if (optionalPerson.isEmpty()){
-            throw new PersonNotFoundException(id);
-        }
-       return personMapper.toDto(optionalPerson.get());
+
+      Person person =  personRepository.findById(id)
+                .orElseThrow(() ->new PersonNotFoundException(id));
+
+       return personMapper.toDto(person);
+    }
+
+
+    public void delete(Long id) throws PersonNotFoundException{
+        verifyIfExists(id);
+
+        personRepository.deleteById(id);
+    }
+
+    private Person verifyIfExists(Long id) throws PersonNotFoundException{
+        return personRepository.findById(id)
+                .orElseThrow(() ->  new PersonNotFoundException(id));
     }
 }
