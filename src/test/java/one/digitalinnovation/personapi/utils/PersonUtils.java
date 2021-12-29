79492f884098987b1210fc15821891496d7531de
oscarmimura <oscar.mimura@gmail.com>
2021-12-29 12:24:51 -0300

    unit test

--- src/test/java/one/digitalinnovation/personapi/utils/PersonUtils.java
+++ src/test/java/one/digitalinnovation/personapi/utils/PersonUtils.java
@@ -10,10 +10,10 @@ import java.util.Collections;
 public class PersonUtils {
 
     private static final String FIRST_NAME = "Oscar ";
-    private static final String LAST_NAME = "Mimura ";
+    private static final String LAST_NAME = "Mimura";
     private static final String CPF_NUMERO = "660.440.640-77";
     private static final Long PERSON_ID = 1L;
-    public static final LocalDate BIRTH_DATE = LocalDate.of(1990, 10, 01);
+    public static final LocalDate BIRTH_DATE = LocalDate.of(1990, 10, 1);
 
     public static PersonDTO createFakeDTO() {
         return PersonDTO.builder()
